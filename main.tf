b4d88f1d98260f247afccd9e52a5da16eb76f1ca
julien tassin <robba.jt@gmail.com>
2023-01-05 17:54:23 +0100

    update vars

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_resource_group" "resourcegroups" {
-    name        = "${var.ResourceGroup}_${var.env}"
+    name        = "${var.ResourceGroup}_${var.environment}"
     location    = var.Location
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "${var.ContainerRegistryName}_${var.env}"
-  resource_group_name = "${var.ResourceGroup}_${var.env}"
+  name                = "${var.ContainerRegistryName}_${var.environment}"
+  resource_group_name = "${var.ResourceGroup}_${var.environment}"
   location            = var.Location
   sku                 = var.ContainerRegistrySKU
   admin_enabled       = false
