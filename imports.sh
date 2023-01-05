e4e72c1d8427892d0e1519db4dcaaa7ce3f7e492
julien tassin <robba.jt@gmail.com>
2023-01-05 16:47:11 +0100

    Remove import

--- imports.sh
+++ imports.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-terraform import azurerm_resource_group fil_rouge_grp
-terraform import azurerm_container_registry filRougeRegistry
+terraform import azurerm_resource_group.fil_rouge_grp
+terraform import azurerm_container_registry.filRougeRegistry
