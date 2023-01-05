00e4603debb4a4062f9b9051fba753564ea77be7
julien tassin <robba.jt@gmail.com>
2023-01-05 14:05:15 +0100

    Mise à jour workflow

--- main.tf
+++ main.tf
@@ -1,5 +1,19 @@
+terraform {
+    required_providers {
+      azurem = {
+        source = "hashicorp/azurem",
+        version = "~> 2.65"
+      }
+    }
+
+    required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+    features {}
+}
+
 resource "azurerm_resource_group" "resourcegroups" {
     name        = var.ResourceGroup
     location    = var.Location
 }
-
