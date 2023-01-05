be280dcbb528d743bc9d9e5d88d4a3fe704d1f81
julien tassin <robba.jt@gmail.com>
2023-01-05 20:41:01 +0100

    test each vars

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "resourcegroups" {
 resource "azurerm_container_registry" "acr" {
     for_each            = toset(var.environments)
     name                = "${var.ContainerRegistryName}${title(each.key)}"
-    resource_group_name = "${var.ResourceGroup}_${var.environment}"
+    resource_group_name = "${var.ResourceGroup}_${each.key}"
     location            = var.Location
     sku                 = var.ContainerRegistrySKU
     admin_enabled       = false
