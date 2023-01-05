87a6d92cc3c18d314ebae4adf01ff2e291e97b91
julien tassin <robba.jt@gmail.com>
2023-01-05 15:44:33 +0100

    Test import before apply

--- import.tf
+++
@@ -1,2 +0,0 @@
-resource "azurerm_resource_group" "fil_rouge_grp" {}
-resource "azurerm_container_registry" "filRougeRegistry" {}
--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-terraform {
-    required_providers {
-      azurerm = {
-        source = "hashicorp/azurerm",
-        version = "~> 2.65"
-      }
-    }
-
-    required_version = ">= 0.14.9"
-}
-
-provider "azurerm" {
-    features {}
-}
-
 resource "azurerm_resource_group" "resourcegroups" {
     name        = var.ResourceGroup
     location    = var.Location
