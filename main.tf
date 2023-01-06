1bf48d34cef37c7c111d48902bc373529d7fc7ac
julien tassin <robba.jt@gmail.com>
2023-01-06 11:27:06 +0100

    fix main

--- main.tf
+++ main.tf
@@ -22,11 +22,14 @@ resource "azurerm_key_vault" "key_vault" {
   name                        = "${var.KeyVaultName}_${each.key}"
   location                    = var.Location
   resource_group_name         = "${var.ResourceGroup}_${each.key}"
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
   enabled_for_disk_encryption = true
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
   sku_name                    = var.KeyVaultSKU
   access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
     key_permissions = [
       "Get",
     ]
