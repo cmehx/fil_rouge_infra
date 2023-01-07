5cf3978caefddec1360f4e07161cf6cd65c30449
julien tassin <robba.jt@gmail.com>
2023-01-07 08:27:45 +0100

    Update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,15 @@
 output "resource_group_names" {
-  for_each = toset(var.environments)
-  value    = azurerm_resource_group.resourcegroups[each.key].name
+  value = azurerm_resource_group.resourcegroups.name
 }
 
 output "kubernetes_acr_names" {
-  for_each = toset(var.environments)
-  value    = azurerm_container_registry.acrs[each.key].name
+  value = azurerm_container_registry.acrs.name
 }
 
 output "azurerm_key_vault_names" {
-  for_each = toset(var.environments)
-  value    = azurerm_key_vault.keyvaults[each.key].name
+  value = azurerm_key_vault.keyvaults.name
 }
 
 output "kubernetes_cluster_names" {
-  for_each = toset(var.environments)
-  value    = azurerm_kubernetes_cluster.clusters[each.key].name
+  value = azurerm_kubernetes_cluster.clusters.name
 }
