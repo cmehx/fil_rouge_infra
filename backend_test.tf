96b8de764270cb7ce811c1d9257e24996b9bfa06
julien tassin <robba.jt@gmail.com>
2023-01-07 19:23:38 +0100

    add storage account

--- backend_test.tf
+++ backend_test.tf
@@ -1,6 +1,8 @@
-backend "azurerm" {
-  resource_group_name  = "fil_rouge_grp_test"
-  storage_account_name = "tfstatefilrougetest"
-  container_name       = "terraformfilrouge"
-  key                  = "terraformfilrouge.tfstate"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "fil_rouge_grp_test"
+    storage_account_name = "tfstatefilrougetest"
+    container_name       = "terraformfilrouge"
+    key                  = "terraformfilrouge.tfstate"
+  }
 }
