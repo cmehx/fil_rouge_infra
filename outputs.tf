3ca97d3825e517696b1a660b76ca5cf673d3e654
julien tassin <robba.jt@gmail.com>
2023-01-07 08:34:04 +0100

    Update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "resource_group_names" {
-  value = azurerm_resource_group.resourcegroups[each.key].name
+  value = values(azurerm_resource_group.resourcegroups).*.name
 }
 
 output "kubernetes_acr_names" {
-  value = azurerm_container_registry.acrs[each.key].name
+  value = values(azurerm_container_registry.acrs).*.name
 }
 
 output "azurerm_key_vault_names" {
-  value = azurerm_key_vault.keyvaults[each.key].name
+  value = values(azurerm_key_vault.keyvaults).*.name
 }
 
 output "kubernetes_cluster_names" {
-  value = azurerm_kubernetes_cluster.clusters[each.key].name
+  value = values(azurerm_kubernetes_cluster.clusters).*.name
 }
