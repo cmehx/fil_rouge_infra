5966861ef2bdd3fdc79ac7fbee0f0ccff985f99a
julien tassin <robba.jt@gmail.com>
2023-01-07 12:30:11 +0100

    Add import script

--- terraform_import.sh
+++ terraform_import.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-environment=$0
-
-terraform import azurerm_resource_group.resourcegroups "fil_rouge_grp_$environment"
+terraform import azurerm_resource_group.resourcegroups "fil_rouge_grp_$TF_VAR_environment"
