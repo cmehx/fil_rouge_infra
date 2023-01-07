da4daea73bea9bf61332823ef5a2586fff648b30
julien tassin <robba.jt@gmail.com>
2023-01-07 19:08:51 +0100

    add storage account

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm",
-      version = "~> 3.0"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-
-  backend "azurerm" {
-    resource_group_name  = "${var.ResourceGroup}_${var.environment}"
-    storage_account_name = "tfstatefilrouge${var.environment}"
-    container_name       = "terraformfilrouge"
-    key                  = "terraformfilrouge.tfstate"
-  }
-}
-
-provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy    = true
-      recover_soft_deleted_key_vaults = true
-    }
-  }
-}
-
-data "azurerm_client_config" "current" {}
 resource "azurerm_resource_group" "resourcegroups" {
   name     = "${var.ResourceGroup}_${var.environment}"
   location = var.Location
