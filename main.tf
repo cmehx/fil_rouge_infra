785397869c302aa7e0882377f0081fa17505469f
julien tassin <robba.jt@gmail.com>
2023-01-07 08:00:31 +0100

    Update cluster def

--- main.tf
+++ main.tf
@@ -76,18 +76,3 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     environment = each.key
   }
 }
-output "resource_group_names" {
-  value = azurerm_resource_group.resourcegroups.name
-}
-
-output "kubernetes_cluster_names" {
-  value = azurerm_container_registry.acrs.name
-}
-
-output "azurerm_key_vault_names" {
-  value = azurerm_key_vault.keyvaults.name
-}
-
-output "kubernetes_cluster_names" {
-  value = azurerm_kubernetes_cluster.clusters.name
-}
